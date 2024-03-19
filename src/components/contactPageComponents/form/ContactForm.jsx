a43b256fa497d459bd669b2519b586f42dd7f84a
Luka Chubinidze <chubinidzel08@gmail.com>
2024-03-19 13:29:33 +0400

    add variable colors; styled contact page

--- src/components/contactPageComponents/form/ContactForm.jsx
+++ src/components/contactPageComponents/form/ContactForm.jsx
@@ -6,22 +6,23 @@ const ContactForm = () => {
     <form className={styles.formFieldsWrapper}>
       <div className={styles.name_email}>
         <div className={styles.inputField}>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" required />
+          <label htmlFor="name">Name *</label>
+          <input placeholder="Example Example" type="text" id="name" name="name" required />
         </div>
         <div className={styles.inputField}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required />
+          <label htmlFor="email">Email *</label>
+          <input placeholder="example@email.com" type="email" id="email" name="email" required />
         </div>
       </div>
       <div className={styles.subject_message}>
         <div className={styles.inputField}>
           <label htmlFor="subject">Subject</label>
-          <input type="text" id="subject" name="subject" />
+          <input placeholder="Write what you want to talk about" type="text" id="subject" name="subject" />
         </div>
         <div className={styles.inputField}>
-          <label htmlFor="question">Question / Message</label>
-          <textarea id="question" name="question" required />
+          <label htmlFor="question">Question / Message *</label>
+          <textarea placeholder="Dear developer..." id="question" name="question" required />
+          <i>* - REQUIRED</i>
         </div>
       </div>
     </form>
