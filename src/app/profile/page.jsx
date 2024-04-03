871807fa78bc3a89321548b8bbecae420b9e8c34
Luka Chubinidze <chubinidzel08@gmail.com>
2024-04-03 17:25:15 +0400

    add loginInfo on profilePage; no styles

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import UserInfo from "../../components/profileComp/userInfo/UserInfo";
+import LoginInfo from "../../components/profileComp/loginInfo/LoginInfo";
 
 const Profile = () => {
   return (
-    <div>
+    <div className="w-full h-full flex flex-col justify-start items-center gap-9">
       <UserInfo />
-      {/* login info
-      <div>
-        <h1 className="text-2xl font-bold">Login Details</h1>
-      </div> */}
+      <LoginInfo />
     </div>
   );
 };
---
+++ src/components/profileComp/loginInfo/NewEmail.jsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { userPrivateInfo } from "../../../data/userObj";
+
+const NewEmail = () => {
+  const [loginInfo, setLoginInfo] = useState(userPrivateInfo);
+  const [isEditing, setIsEditing] = useState();
+  const [inputVal, setInputVal] = useState("");
+
+  useEffect(() => {
+    if (inputVal === "" || !inputVal.includes("@") || !inputVal.includes(".")) {
+      setIsEditing(false);
+    } else {
+      setIsEditing(true);
+    }
+  }, [inputVal]);
+
+  const handleSave = () => {
+    setLoginInfo((prev) => ({ ...prev, email: inputVal }));
+    setInputVal("");
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="w-full flex justify-between">
+        <i className="opacity-60">Email</i>
+        <p>
+          Current:{" "}
+          <span className="italic">
+            {loginInfo.email}{" "}
+            {isEditing && (
+              <button
+                className="bg-slate-500 text-white rounded-full px-3"
+                onClick={handleSave}
+              >
+                Save
+              </button>
+            )}
+          </span>
+        </p>
+      </div>
+      <input
+        className="w-full text-base border-2 border-black rounded-md px-3 py-2"
+        type="email"
+        placeholder="Enter new email"
+        value={inputVal}
+        onChange={(e) => setInputVal(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default NewEmail;
