4d7b5b3909b745d9ee1d99c61293cd3a54381225
Luka Chubinidze <chubinidzel08@gmail.com>
2024-06-12 16:16:17 +0400

    add new comment textarea input

--- src/app/[locale]/dashboard/(authenticated)/announcements/[slug]/page.tsx
+++ src/app/[locale]/dashboard/(authenticated)/announcements/[slug]/page.tsx
@@ -13,6 +13,17 @@ export default function AnnouncementPage({ params }: Props) {
     <div className="w-full min-h-screen flex flex-col items-center px-3 md:px-7 py-3 gap-5">
       <Details />
       <hr className="w-full max-w-4xl border-2 rounded-full mt-12" />
+      <div className="w-full max-w-4xl flex flex-col items-end gap-3">
+        <h1 className="w-full max-w-4xl font-bold text-2xl">Add new comment</h1>
+        <textarea
+          name="announcement_new_comment"
+          id="announcement_new_comment"
+          placeholder="What is in your mind..."
+          rows={5}
+          className="w-full bg-transparent border-2 rounded-lg focus:outline-none p-3 resize-none"
+        />
+        <button className="px-3 py-1 rounded-lg bg-red-400">Add Comment</button>
+      </div>
       <Comments />
     </div>
   );
--- src/components/dashboard/announcements_page/single_page/comments/Comments.tsx
+++ src/components/dashboard/announcements_page/single_page/comments/Comments.tsx
@@ -5,7 +5,7 @@ export default function Comments() {
   return (
     <>
       <div className="w-full max-w-4xl flex justify-between items-center">
-        <h1 className="w-full max-w-4xl font-bold text-2xl">Comments</h1>
+        <h1 className="w-full max-w-4xl font-bold text-2xl">All Comments</h1>
         <Sorter />
       </div>
       <List />
