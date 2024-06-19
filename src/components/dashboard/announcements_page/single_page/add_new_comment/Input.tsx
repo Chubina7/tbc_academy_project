151c4f76894fe2be3dda72bea8c9dc87de06a684
Luka Chubinidze <chubinidzel08@gmail.com>
2024-06-19 12:48:35 +0400

    handled comment addition in announcements

--- src/components/dashboard/announcements_page/single_page/add_new_comment/Input.tsx
+++ src/components/dashboard/announcements_page/single_page/add_new_comment/Input.tsx
@@ -1,4 +1,18 @@
-export default function Input() {
+interface Props {
+  state: [string, React.Dispatch<React.SetStateAction<string>>];
+  handleSubmit: () => void;
+  isDisabled: boolean;
+  errorState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Input({
+  state,
+  handleSubmit,
+  isDisabled,
+  errorState,
+}: Props) {
+  const [value, setValue] = state;
+
   return (
     <textarea
       name="announcement_new_comment"
@@ -6,6 +20,18 @@ export default function Input() {
       placeholder="What is in your mind..."
       rows={5}
       className="w-full bg-transparent border-2 rounded-lg focus:outline-none p-3 resize-none"
+      value={value}
+      onChange={(e) => {
+        errorState(false);
+        setValue(e.target.value);
+      }}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          handleSubmit();
+        }
+      }}
+      disabled={isDisabled}
     />
   );
 }
