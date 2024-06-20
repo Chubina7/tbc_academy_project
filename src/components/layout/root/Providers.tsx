5381010ca904a32cb943d60d28c88d13a047d881
Luka Chubinidze <chubinidzel08@gmail.com>
2024-06-20 15:43:58 +0400

    refactored notifications context and UI

--- src/components/layout/root/Providers.tsx
+++ src/components/layout/root/Providers.tsx
@@ -1,6 +1,5 @@
 import { AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
 import NotificationsProvider from "../../../context/providers/NotificationsProvider";
-import Notification from "../../dashboard/notifications/Notification";
 
 interface Props {
   children: React.ReactNode;
@@ -10,7 +9,6 @@ interface Props {
 export default function Providers({ children, intlProp }: Props) {
   return (
     <NotificationsProvider>
-      <Notification />
       <NextIntlClientProvider messages={intlProp}>
         {children}
       </NextIntlClientProvider>
