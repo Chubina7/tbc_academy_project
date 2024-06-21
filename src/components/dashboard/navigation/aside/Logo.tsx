849700f65fdc592a807b0e6abb4cbb635478c320
Luka Chubinidze <chubinidzel08@gmail.com>
2024-06-21 09:16:42 +0400

    change logo and name of application

--- src/components/dashboard/navigation/aside/Logo.tsx
+++ src/components/dashboard/navigation/aside/Logo.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import React from "react";
-import { GiWhiteBook } from "react-icons/gi";
+import { IoSchool } from "react-icons/io5";
 import TopToDown from "../../../ui/framerMotionDivs/directions/TopToDown";
 
 function Logo() {
   return (
     <Link href={"/dashboard"}>
       <TopToDown className="flex justify-center items-center gap-3">
-        <GiWhiteBook size={36} />
-        <h1 className="hidden xl:block text-lg font-bold">LMS For you</h1>
+        <IoSchool size={36} />
+        <h1 className="hidden xl:block text-lg font-bold">Teach-Flow</h1>
       </TopToDown>
     </Link>
   );
